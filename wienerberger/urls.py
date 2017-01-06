8c32be3bf5dea002f7da65d904306f7d3443adf8
Andrew <lyapkoandy13@gmail.com>
2017-01-06 15:24:29 +0200

    fixed urls
    fixed urls

--- wienerberger/urls.py
+++ wienerberger/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^create_user', views.create_user, name="create_user"),
-	url(r'^send_message', views.send_message, name="send_message"),
-	url(r'^delete_user', views.delete_user, name="delete_user"),
-	url(r'^grant_access', views.grant_access, name="grant_access"),
-	url(r'^deny_access', views.deny_access, name="deny_access"),
+	url(r'^create_user/$', views.create_user, name="create_user"),
+	url(r'^send_message/', views.send_message, name="send_message"),
+	url(r'^delete_user/', views.delete_user, name="delete_user"),
+	url(r'^grant_access/', views.grant_access, name="grant_access"),
+	url(r'^deny_access/', views.deny_access, name="deny_access"),
 ]
