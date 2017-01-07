ac09578da124767c8cc17e3d6369bef2aece7c70
Andrew <lyapkoandy13@gmail.com>
2017-01-07 13:24:31 +0200

    fixed serialization
    fixed serialization

--- wienerberger/views.py
+++ wienerberger/views.py
@@ -79,5 +79,5 @@ def deny_access(request):
 @csrf_exempt
 def get_users(request):
 	a = WienerbergerUser.objects.all()
-	json_data = serializers.serialize('json', list(a))
+	json_data = serializers.serialize('json', list(a), fields=('id','firstname','secondname','token','auth','date'))
 	return JsonResponse(json_data, safe=False)
