50ec2f1c92847d829fc0b4df49c3d54779f05bdd
Andrew <lyapkoandy13@gmail.com>
2017-04-09 14:06:33 +0300

    fixed ajax
    fixed ajax

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -3,6 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+ 
 var url = "/AITS/";
 var showMenu = false;
 $(document).ready(function() {
